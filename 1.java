86312a3cbe01ed433db3b3c60f031c4c63b2d2a8
vikrantsingh2105 <vikrant.thakur3221@gmail.com>
2026-07-23 15:35:33 +0530

    Add Student class with biodata output

---
+++ 1.java
@@ -0,0 +1,22 @@
+class Student 
+{public static void main (String[]arg)
+ {
+     System.out.println("---BIODATA---");
+    System.out.println("NAME\t\t:VIKRANT SINGH");
+    System.out.println("AGE\t\t:19");
+    System.out.println("GENDER\t\t:M");
+    System.out.println("CITY\t\t:REWARI");
+
+
+System.out.println("---formatted output---");
+String name ="VIKRANT SINGH";
+int age =19;
+String GENDER="MALE";
+
+System.out.printf("Name: %s%n",name);
+System.out.printf("Age: %d%n",age);
+
+
+
+}
+}
