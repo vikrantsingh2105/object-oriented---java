50ea2c5e925200e7dc8564134c7d8d5ac260c12a
vikrantsingh2105 <vikrant.thakur3221@gmail.com>
2026-07-29 22:12:48 +0530

    Rename file from 04.largestn.java to 05.largestnum.java

--- 05.largestnum.java
+++ 05.largestnum.java
@@ -1,9 +1,17 @@
+import java.util.Scanner
 class Largest{
     public static void main(String[]args){
         
-        int a=70;
-        int b=30;
-        int c=45;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter First Number : ");
+        int a = sc.nextInt();
+        
+        System.out.print("Enter Second Number : ");
+        int b = sc.nextInt();
+        
+        System.out.print("Enter Third Number : ");
+        int c = sc.nextInt();
         
         if(a>b && a>c){
             System.out.println("Largest Number is : " + a);
