7f6f6816a62e5c115a97cd15d5bb9103a7c53e7c
vikrantsingh2105 <vikrant.thakur3221@gmail.com>
2026-07-30 15:10:50 +0530

    Add pattern printing program in 11.pattern.java

---
+++ 11.pattern.java
@@ -0,0 +1,17 @@
+class Pattern
+{
+    public static void main(String[]arg)
+    {
+        int i;
+        int j;
+        
+        for (i=1;i<=4;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+        System.out.println(" ");    
+        }
+    }
+}
