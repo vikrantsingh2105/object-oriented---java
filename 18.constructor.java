5cddab2f1ffca4d58d4d6f9c8de87a350f2027a8
vikrantsingh2105 <vikrant.thakur3221@gmail.com>
2026-08-07 09:51:27 +0530

    Add Student class with constructor and display method

---
+++ 18.constructor.java
@@ -0,0 +1,21 @@
+class Student{
+    int roll_no;
+    String name;
+    static String college = "Sharda";
+    Student(int r,String n) {
+         
+         roll_no=r;
+         name=n;
+     }
+     void display(){
+        System.out.println(roll_no+" "+name+" "+college);
+    }
+    public static void main(String[]args){
+        Student st = new Student(23,"abc");
+        st.display();
+        Student st1 = new Student(29,"hdk");
+        st1.display();
+        Student st2 = new Student(30,"jkl");
+        st2.display();
+    }
+}
